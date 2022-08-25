6580377dec090b3797bd0b7815659000cec6b8ed
苏晓银(220523201) <suxiaoyin@dianliantech.com>
2022-08-25 18:26:27 +0800

    updated and all passed

--- SCF/case_api/scfAllocateSubsidiaries.py
+++ SCF/case_api/scfAllocateSubsidiaries.py
@@ -99,21 +99,8 @@ def api_scfAllocateSubsidiaries_queryPage(token, payload):
     print(f'接口响应为：{r.text}')
     return r
 
-def api_scfAllocateSubsidiaries_delete(token, payload):
-    """删除"""
-    url = f'{api_host}/api-scf/scfAllocateSubsidiaries/delete'
-    headers = {
-        "Content-Type": "application/json;charset=UTF-8",
-        "x-appid-header": "2",
-        "Authorization": token
-    }
-    r = requests.get(url, headers=headers, data=json.dumps(payload))
-    print(f'请求地址：{url}')
-    print(f'请求头：{headers}')
-    print(f'请求参数：{payload}')
-    print(f'接口响应为：{r.text}')
-    return r
 
+g_d = {}
 
 class ScfAllocateSubsidiaries(unittest.TestCase):
     def test_001_scfAllocateSubsidiaries_queryAll(self):
@@ -145,6 +132,7 @@ class ScfAllocateSubsidiaries(unittest.TestCase):
         }
         r = api_scfAllocateSubsidiaries_insert(token_scf_enterprise, payload)
         r_json = r.json()
+        g_d['id'] = r_json['datas']
         restime_now = r.elapsed.total_seconds()
         customize_dict['restime_now'] = restime_now
         self.assertEqual(200, r_json['resp_code'])
@@ -208,15 +196,4 @@ class ScfAllocateSubsidiaries(unittest.TestCase):
         self.assertEqual('SUCCESS', r_json['resp_msg'])
         self.assertLessEqual(restime_now, restime)
 
-    def test_007_scfAllocateSubsidiaries_delete(self):
-        """【核心企业】删除"""
-        payload = {
-            "id": 0
-        }
-        r = api_scfAllocateSubsidiaries_delete(token_scf_enterprise, payload)
-        r_json = r.json()
-        restime_now = r.elapsed.total_seconds()
-        customize_dict['restime_now'] = restime_now
-        self.assertEqual(200, r_json['resp_code'])
-        self.assertEqual('SUCCESS', r_json['resp_msg'])
-        self.assertLessEqual(restime_now, restime)
+
