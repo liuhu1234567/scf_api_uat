baf1bd84dba0a4a95f7e3068a7200eae39637342
220901202 <ll@123456>
2022-09-20 17:26:22 +0800

    报告优化20220920

--- SCF/testdemo.py
+++ SCF/testdemo.py
@@ -1,23 +1,23 @@
 import requests
 import unittest
-from XTestRunner import HTMLTestRunner
-from XTestRunner.config import RunResult
+# from XTestRunner import HTMLTestRunner
+from common.TestRunner import HTMLTestRunner
 
 class YouTest(unittest.TestCase):
+
 
     def test_get(self):
         """测试get接口 """
         # r = requests.get("https://httpbin.org/get", params={"key":"value"})
         # print(r.json())
-        self.assertEqual(1+1,2)
-
+        # self.assertEqual(1+1,2)
         self.assertLessEqual(4, 3,'Test api timeout')
 
     def test_post(self):
         """测试post接口 """
         # r = requests.post("https://httpbin.org/post", data={"key":"value"})
         # print(r.json())
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 1, 3)
         # self.assertLessEqual(4, 3, 'Test api timeout！')
 
     def test_put(self):
@@ -25,7 +25,21 @@ class YouTest(unittest.TestCase):
         # r = requests.put("https://httpbin.org/put", data={"key":"value"})
         # print(r.json())
         self.assertEqual(1 + 1, 2)
+        raise Exception
 
+    def test_pass(self):
+        """测试put接口 """
+        # r = requests.put("https://httpbin.org/put", data={"key":"value"})
+        # print(r.json())
+        self.assertEqual(1 + 1, 2)
+
+    def test_pass2(self):
+        """测试put接口 """
+        # r = requests.put("https://httpbin.org/put", data={"key":"value"})
+        # print(r.json())
+        self.assertEqual(1 + 1, 2)
+
+    @unittest.skip('  ')
     def test_delete(self):
         """测试delete接口 """
         # r = requests.delete("https://httpbin.org/delete", data={"key":"value"})
@@ -34,7 +48,8 @@ class YouTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    report = "../result/api_result1.html"
+    # HTMLTestRunner
+    report = "./result/api_result5.html"
     with(open(report, 'wb')) as fp:
         unittest.main(testRunner=HTMLTestRunner(
             stream=fp,
