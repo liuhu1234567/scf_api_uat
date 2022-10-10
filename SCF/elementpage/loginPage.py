a6f9e432c1f22ca55be9b5bd70b6a27fc861ad54
220901202 <ll@123456>
2022-10-10 14:20:14 +0800

    uidemo-1011

--- SCF/elementpage/loginPage.py
+++ SCF/elementpage/loginPage.py
@@ -11,11 +11,22 @@ class loginPage(BasePage):
         self.send_keys(self.username_element, username)
         return username
 
-    def password_input(self, username):
-        self.send_keys(self.username_element, username)
-        return username
+    def password_input(self, password):
+        self.send_keys(self.password_element, password)
+        return password
+
+    def code_input(self, code):
+        self.send_keys(self.code_element, code)
+        return code
+
+    def loginButton_clike(self):
+        self.clike(self.loginButton_element)
+
 
 
 obj = loginPage()
 obj.open('https://uat-cloud.dianliantech.com/user/login')
 obj.user_input('AEW6317585245519')
+obj.password_input('Aa1234567')
+obj.code_input('1234')
+obj.loginButton_clike()
