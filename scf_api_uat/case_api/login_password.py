7f5ddb7016c2ec87cc57bfdbcddff8d9c8b7113a
220106009 <liuhu@dianliantech.com>
2022-11-07 17:28:01 +0800

    11-7-2 修改uat账号

--- scf_api_uat/case_api/login_password.py
+++ scf_api_uat/case_api/login_password.py
@@ -22,22 +22,22 @@ from Crypto.PublicKey import RSA
 #     return r
 
 
-def encrypt(pwd):
-    """RSA加密"""
-    public_key = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKSItqTh99pxpM9iRuqtFy/f81g3hTXCdGMPjWGgMTziNMxK60BmzOqw24bjXQQ+pgKQhSsPhxT4QsgaG0QbDE8CAwEAAQ=="
-    key = '-----BEGIN PUBLIC KEY-----\n' + public_key + '\n-----END PUBLIC KEY-----'
-    # print('key---------------------')
-    # print(key)
-    rsakey = RSA.importKey(key)
-    cipher = Cipher_pksc1_v1_5.new(rsakey)
-    encrypt_text = cipher.encrypt(pwd.encode())
-    r = base64.b64encode(encrypt_text).decode()
-    return r
+# def encrypt(pwd):
+#     """RSA加密"""
+#     public_key = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKSItqTh99pxpM9iRuqtFy/f81g3hTXCdGMPjWGgMTziNMxK60BmzOqw24bjXQQ+pgKQhSsPhxT4QsgaG0QbDE8CAwEAAQ=="
+#     key = '-----BEGIN PUBLIC KEY-----\n' + public_key + '\n-----END PUBLIC KEY-----'
+#     # print('key---------------------')
+#     # print(key)
+#     rsakey = RSA.importKey(key)
+#     cipher = Cipher_pksc1_v1_5.new(rsakey)
+#     encrypt_text = cipher.encrypt(pwd.encode())
+#     r = base64.b64encode(encrypt_text).decode()
+#     return r
 
 
 def api_login_password(payload):
     """用户名密码登录"""
-    url = 'https://uat-gateway.dianliantech.com/api-uaa/login/password'
+    url = 'https://disosuat.dianliantech.com/api-uaa/login/password'
     headers = {
         'Content-Type': 'application/json;charset=UTF-8',
         'Accept-Language': 'zh-CN,zh;q=0.9'
@@ -54,7 +54,7 @@ class LoginPassword(unittest.TestCase):
 
     def test_login_password(self):
         """账号正确，密码正确"""
-        password = encrypt("Ss123456")
+        # password = encrypt("Ss123456")
         # payload = {
         #     "username": "ZVXS17585245519",
         #     "password": password,
@@ -67,7 +67,7 @@ class LoginPassword(unittest.TestCase):
         # }
         payload = {
             "username": "ML4W17585245519",
-            "password": password,
+            "password": "Aa123456",
             "code": "是",
             "grant_type": "password",
             "clientId": "webApp",
--- scf_api_uat/common/get_token.py
+++ scf_api_uat/common/get_token.py
@@ -1,19 +1,18 @@
-from case_api.login_password import api_login_password, encrypt
+from case_api.login_password import api_login_password
 from common.do_config import scf_financier, scf_enterprise, scf_supplier, scf_platform, scf_factor, scf_subsidiaries
 
 
 def get_scf_financier():
     """获取资金方账号token"""
-    password = encrypt(scf_financier['password'])
+    # password = encrypt(scf_financier['password'])
     data = {
         "username": scf_financier['username'],
-        "password": password,
-        "code": "是",
+        "password": scf_financier['password'],
         "grant_type": "password",
         "clientId": "webApp",
         "clientSecret": "webApp",
-        "key": "",
-        "type": 1}
+        "isVerify": True
+    }
     r_json = api_login_password(data).json()
     # print(r_json)
     token = f"{r_json['datas']['token_type']} {r_json['datas']['access_token']}"
@@ -23,16 +22,15 @@ def get_scf_financier():
 
 def get_scf_enterprise():
     """获取核心企业账号token"""
-    password = encrypt(scf_enterprise['password'])
+    # password = encrypt(scf_enterprise['password'])
     data = {
         "username": scf_enterprise['username'],
-        "password": password,
-        "code": "是",
+        "password": scf_enterprise['password'],
         "grant_type": "password",
         "clientId": "webApp",
         "clientSecret": "webApp",
-        "key": "",
-        "type": 1}
+        "isVerify": True
+    }
     r_json = api_login_password(data).json()
     token = f"{r_json['datas']['token_type']} {r_json['datas']['access_token']}"
     # token = {'token_type': r_json['datas']['token_type'], 'access_token': r_json['datas']['access_token']}
@@ -41,16 +39,15 @@ def get_scf_enterprise():
 
 def get_scf_supplier():
     """获取供应商/经销商账号token"""
-    password = encrypt(scf_supplier['password'])
+    # password = encrypt(scf_supplier['password'])
     data = {
         "username": scf_supplier['username'],
-        "password": password,
-        "code": "是",
+        "password": scf_supplier['password'],
         "grant_type": "password",
         "clientId": "webApp",
         "clientSecret": "webApp",
-        "key": "",
-        "type": 1}
+        "isVerify": True
+    }
     r_json = api_login_password(data).json()
     token = f"{r_json['datas']['token_type']} {r_json['datas']['access_token']}"
     # token = {'token_type': r_json['datas']['token_type'], 'access_token': r_json['datas']['access_token']}
@@ -59,16 +56,15 @@ def get_scf_supplier():
 
 def get_scf_platform():
     """获取平台方账号token"""
-    password = encrypt(scf_platform['password'])
+    # password = encrypt(scf_platform['password'])
     data = {
         "username": scf_platform['username'],
-        "password": password,
-        "code": "是",
+        "password": scf_platform['password'],
         "grant_type": "password",
         "clientId": "webApp",
         "clientSecret": "webApp",
-        "key": "",
-        "type": 1}
+        "isVerify": True
+    }
     r_json = api_login_password(data).json()
     token = f"{r_json['datas']['token_type']} {r_json['datas']['access_token']}"
     # token = {'token_type': r_json['datas']['token_type'], 'access_token': r_json['datas']['access_token']}
@@ -77,16 +73,15 @@ def get_scf_platform():
 
 def get_scf_subsidiaries():
     """获取核企子公司账号token"""
-    password = encrypt(scf_subsidiaries['password'])
+    # password = encrypt(scf_subsidiaries['password'])
     data = {
         "username": scf_subsidiaries['username'],
-        "password": password,
-        "code": "是",
+        "password": scf_subsidiaries['password'],
         "grant_type": "password",
         "clientId": "webApp",
         "clientSecret": "webApp",
-        "key": "",
-        "type": 1}
+        "isVerify": True
+    }
     r_json = api_login_password(data).json()
     token = f"{r_json['datas']['token_type']} {r_json['datas']['access_token']}"
     # token = {'token_type': r_json['datas']['token_type'], 'access_token': r_json['datas']['access_token']}
@@ -95,27 +90,27 @@ def get_scf_subsidiaries():
 
 def get_scf_factor():
     """获取保理商账号token"""
-    password = encrypt(scf_factor['password'])
+    # password = encrypt(scf_factor['password'])
     # print('set----------------------------------------')
     # print(password)
     data = {
         "username": scf_factor['username'],
-        "password": password,
-        "code": "是",
+        "password": scf_factor['password'],
         "grant_type": "password",
         "clientId": "webApp",
         "clientSecret": "webApp",
-        "key": "",
-        "type": 1}
+        "isVerify": True
+    }
     r_json = api_login_password(data).json()
     token = f"{r_json['datas']['token_type']} {r_json['datas']['access_token']}"
     # token = {'token_type': r_json['datas']['token_type'], 'access_token': r_json['datas']['access_token']}
     return token
 
+
 token_scf_financier = get_scf_financier()
 token_scf_enterprise = get_scf_enterprise()
 token_scf_supplier = get_scf_supplier()
-token_scf_platform = get_scf_platform()
+# token_scf_platform = get_scf_platform()
 token_scf_factor = get_scf_factor()
 token_scf_subsidiaries = get_scf_subsidiaries()
 
@@ -123,6 +118,6 @@ if __name__ == '__main__':
     print(token_scf_financier)
     print(token_scf_enterprise)
     print(token_scf_supplier)
-    print(token_scf_platform)
+    # print(token_scf_platform)
     print(token_scf_factor)
     print(token_scf_subsidiaries)
